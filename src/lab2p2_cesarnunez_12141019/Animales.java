3bd6ddf3089172f50097a7efad3b5d4e572e8b84
César Núñez <cesar23augusto16@gmail.com>
2022-01-28 13:57:20 -0600

    Clase de animales

--- src/lab2p2_cesarnunez_12141019/Animales.java
+++ src/lab2p2_cesarnunez_12141019/Animales.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab2p2_cesarnunez_12141019;
 
-/**
- *
- * @author cesar
- */
+
 public class Animales {
     
 }

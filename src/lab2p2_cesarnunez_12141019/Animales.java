256e5ed758dcc642e49be779b0a0e5917f1f3a7d
César Núñez <cesar23augusto16@gmail.com>
2022-01-28 15:02:31 -0600

    Agregar objetos de animales

--- src/lab2p2_cesarnunez_12141019/Animales.java
+++ src/lab2p2_cesarnunez_12141019/Animales.java
@@ -4,4 +4,91 @@ package lab2p2_cesarnunez_12141019;
 
 public class Animales {
     
+    private String nCientifico;
+    private String nComun;
+    private String habitat;
+    private String alimentacion;
+    private String rasgos; // descripcion de rasgos
+    private String geografica; // distribución geográfica
+    private int vida;
+
+    public Animales(String nCientifico, String nComun, String habitat, String alimentacion, String rasgos, String geografica, int vida) {
+        this.nCientifico = nCientifico;
+        this.nComun = nComun;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.rasgos = rasgos;
+        this.geografica = geografica;
+        this.vida = vida;
+    }
+    
+    public Animales(){}
+
+    public String getnCientifico() {
+        return nCientifico;
+    }
+
+    public void setnCientifico(String nCientifico) {
+        this.nCientifico = nCientifico;
+    }
+
+    public String getnComun() {
+        return nComun;
+    }
+
+    public void setnComun(String nComun) {
+        this.nComun = nComun;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getRasgos() {
+        return rasgos;
+    }
+
+    public void setRasgos(String rasgos) {
+        this.rasgos = rasgos;
+    }
+
+    public String getGeografica() {
+        return geografica;
+    }
+
+    public void setGeografica(String geografica) {
+        this.geografica = geografica;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre científico " + nCientifico + 
+                ", Nombre común " + nComun +
+                ", Hábitat " + habitat +
+                ", Alimentación " + alimentacion +
+                ", Descripción de rasgos " + rasgos +
+                ", Distribución geográfica " + geografica +
+                ", Vida " + vida;                
+    }
+    
 }
--- src/lab2p2_cesarnunez_12141019/Lab2P2_CesarNunez_12141019.java
+++ src/lab2p2_cesarnunez_12141019/Lab2P2_CesarNunez_12141019.java
@@ -1,9 +1,117 @@
 package lab2p2_cesarnunez_12141019;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab2P2_CesarNunez_12141019 {
+    
+    private static ArrayList<Animales> regAnimales = new ArrayList<>();
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         
+        // animales por defecto 
+        regAnimales.add(new Animales("Panthera pardus", "Leopardo", "selvas tropicales", "animales de medio tamaño", 
+                "tiene manchas y puede rugir", "África, Oriente Medio y Asia", 20));
+        regAnimales.add(new Animales("Canis lupus", "Lobo", "selvas y témpanos de hielo", "carne", 
+                "colmillos y cola larga", "Eurasia y América del Norte", 7));
+        regAnimales.add(new Animales("Aptenodytes forsteris", "Pinguino Eperador", "Zonas heladas", "peces", 
+                "Aves blancas y negras que no vuelan", "Antártida", 17));
+        
+        // menu
+        while(true){
+            switch(menu()){
+                case 1:{                    
+                    System.out.print("Nombre científico: ");
+                    String nci;
+                    do{
+                        nci = input.nextLine();
+                    }while(regAnimales.contains(nci));
+                    System.out.print("Nombre común: ");
+                    String nc = input.nextLine();
+                    System.out.print("Hábitat: ");
+                    String ha = input.nextLine();
+                    System.out.print("Alimentación: ");
+                    String al = input.nextLine();
+                    System.out.print("Rasgos descriptivos: ");
+                    String rd = input.nextLine();
+                    System.out.print("Distribución geográfica: ");
+                    String dg = input.nextLine();
+                    System.out.print("Edad: ");
+                    int ed = input.nextInt();
+                    regAnimales.add(new Animales(nci, nc, ha, al, rd, dg, ed));                    
+                    break;
+                }
+
+                case 2:{ // listar animales
+                    System.out.print("1) Por posición\n"
+                            + "2) Todos los animales\n"
+                            + "3) Por nombre científico\n"
+                            + "Ingrese una opción: ");
+                    int opcion = input.nextInt();
+                    
+                    switch(opcion){
+                        case 1:{
+                            System.out.print("Ingrese la posición: ");
+                            int pos = input.nextInt();                            
+                            System.out.println((regAnimales.get(pos)).toString());
+                            break;
+                        }
+                        
+                        case 2:{
+                            for (Animales regAnimale : regAnimales) {
+                                System.out.println(regAnimale.toString());
+                            }
+                            break;
+                        }
+                        
+                        case 3:{
+                            System.out.print("Nombre científico: ");
+                            //for (Animales regAnimale : regAnimales) {
+                                //if(regAnimale.equals(nc))
+                            //}
+                            break;
+                        }
+                    }
+                    break;
+                }
+
+                case 3:{
+
+                    break;
+                }
+
+                case 4:{
+
+                    break;
+                }
+
+                case 5:{
+
+                    break;
+                }
+
+                case 6:{
+                    System.exit(0);
+                    break;
+                }
+
+                default:{
+                    System.out.println("Ingrese una opción correcta!");
+                }                
+            } // switch
+        } // while
+        
+    } // main
+    
+    public static int menu(){
+        System.out.println("1) Agregar animal\n"
+                        + "2) Listar animales\n"
+                        + "3) Modificar animales\n"
+                        + "4) Alimentar animales\n"
+                        + "5) Eliminar animal\n"
+                        + "6) Salir");
+        return input.nextInt();
     }
     
 }
